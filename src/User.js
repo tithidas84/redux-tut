c53fd3d01726c081365cfa5e88722c3959825418
Dutta, Anupallab <anupallab.dutta@united.com>
2022-09-12 00:00:05 -0500

    Add to cart design done

---
+++ src/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+const User = (props)=>{
+    const {data} = props;
+    return(
+        <div>
+            <h1>User Component</h1>
+            <h4>{data.name}</h4>
+        </div>
+    )
+}
+
+export default User;
---
+++ src/container/HomeContainer.js
@@ -0,0 +1,11 @@
+
+import Home from "../components/Home";
+
+function HomeContainer(){
+
+    return(
+        <div><Home /></div>
+    )
+}
+
+export default HomeContainer;
